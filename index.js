6a076aef137dc5eb394e6f7ae9787c4dd6b1a8bb
Colin Meret <colinmeret@gmail.com>
2015-05-09 19:50:05 -0400

    firebase to update prompts

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ var app = angular.module("Orange2Orange", ["opentok", "firebase"]);
 
 app.controller("PromptCtrl", ["$scope",
     function ($scope) {
+
+        var ref = new Firebase("https://burning-fire-1005.firebaseio.com/prompt");
+
+        ref.on('value', function(snapshot) {
+          $scope.randoPrompt = snapshot.exists() ? snapshot.val() : "Example prompt: Dance like nobody is watching!";
+          console.log($scope.randoPrompt);
+        });
+
         var prompts = [
             "Without saying 'chopper' give us your best Arnold impersonation!",
             "Show us how you act when you're bored.",
             "What superpower would you give your mother?"
         ];
-        var number = Math.floor(Math.random() * prompts.length);
-        $scope.randoPrompt = prompts[number];
+
+        $scope.getPrompt = function() {
+          var number = Math.floor(Math.random() * prompts.length);
+          ref.set(prompts[number]);
+        };
 
     }
 ]);

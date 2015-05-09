3007d40aff4ae0d5453da3f5f37678530d24884a
Wesley Robinson <Robinson.WesleyD@gmail.com>
2015-05-09 19:51:22 -0400

    merge

--- roundsController.js
+++ roundsController.js
@@ -1,16 +1,21 @@
-app.controller("RoundCtrl", ["$scope", "$rootScope",
-    function ($scope, $rootScope) {
+app.controller("RoundCtrl", ["$scope", "$rootScope", "$firebaseArray",
+    function ($scope, $rootScope, $firebaseArray) {
         var counter = 0;
-        var myDataRef = new Firebase('https://burning-fire-1005.firebaseio.com/');
+        var myDataRef = new Firebase('https://burning-fire-1005.firebaseio.com/videos');
 
-        myDataRef.push({
-            round : counter,
-            prompt: $rootScope.randoPrompt,
-            videos: [],
-            winner: ''
+        var myArr = $firebaseArray(myDataRef);
+
+        myArr.$add({user: "Wesley", video: "newtoken234567"}).then(function(ref) {
+            var id = ref.key();
+            console.log("added record with id " + id);
+            myArr.$indexFor(id); // returns location in the array
         });
 
+        $rootScope.videos = myArr;
 
+        myArr.on('child_added', function () {
+
+        });
 
     }
 ]);
